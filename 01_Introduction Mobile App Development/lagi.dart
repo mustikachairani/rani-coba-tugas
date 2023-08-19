2115ab6c0e17a03225d58214400a0d928b127b16
mustika chairani <mustikachrn5@gmail.com>
2023-08-19 18:41:05 +0700

    launch

---
+++ 01_Introduction Mobile App Development/lagi.dart
@@ -0,0 +1,11 @@
+int nim = 2101092959;
+String nama = 'rani';
+String jenis_kelamin = 'perempuan';
+double berat_badan = 50.5;
+
+void main(){
+  print('hallo nama aku $nama');
+  print('jenis kelamin saya $jenis_kelamin');
+  print('nim aku $nim');
+  print('berat badanku $berat_badan');
+}
